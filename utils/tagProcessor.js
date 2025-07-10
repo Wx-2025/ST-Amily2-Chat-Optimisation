7ff95c127fdb9f5893b4cc291e3fe6b74c03a96a
Wx-2025 <351320169@qq.com>
2025-07-10 14:37:05 +0800

    Create tagProcessor.js

---
+++ utils/tagProcessor.js
@@ -0,0 +1,14 @@
+function extractContentByTag(xmlString, tagName) {
+  const regex = new RegExp(`<${tagName}[^>]*>([\\s\\S]*?)<\\/${tagName}>`);
+  const match = xmlString.match(regex);
+  return match ? match[1] : null;
+}
+
+function replaceContentByTag(xmlString, tagName, newContent) {
+  const regex = new RegExp(`(<${tagName}[^>]*>)[\\s\\S]*?(<\\/${tagName}>)`);
+  if (regex.test(xmlString)) {
+    return xmlString.replace(regex, `$1${newContent}$2`);
+  }
+  return xmlString;
+}
+export { extractContentByTag, replaceContentByTag };
