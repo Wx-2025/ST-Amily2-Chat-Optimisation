3e3621ba443ea6618b4f72f8735e02151ac2dadd
Wx-2025 <351320169@qq.com>
2025-07-12 17:47:23 +0800

    Update tagProcessor.js

--- utils/tagProcessor.js
+++ utils/tagProcessor.js
@@ -4,6 +4,12 @@ function extractContentByTag(xmlString, tagName) {
   return match ? match[1] : null;
 }
 
+function extractFullTagBlock(xmlString, tagName) {
+  const regex = new RegExp(`(<${tagName}[^>]*>[\\s\\S]*?<\\/${tagName}>)`);
+  const match = xmlString.match(regex);
+  return match ? match[0] : null;
+}
+
 function replaceContentByTag(xmlString, tagName, newContent) {
   const regex = new RegExp(`(<${tagName}[^>]*>)[\\s\\S]*?(<\\/${tagName}>)`);
   if (regex.test(xmlString)) {
@@ -11,4 +17,5 @@ function replaceContentByTag(xmlString, tagName, newContent) {
   }
   return xmlString;
 }
-export { extractContentByTag, replaceContentByTag };
+
+export { extractContentByTag, replaceContentByTag, extractFullTagBlock };
